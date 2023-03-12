469a8a670dcd041c2a7d5cb56febef51340a8015
Sahil Chhabra <lastspin290901@gmail.com>
2023-03-13 01:21:44 +0530

    employee management app

---
+++ employee-management-system/src/main/java/com/springbackendapplication/employee/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.springbackendapplication.employee.repository;
+
+import com.springbackendapplication.employee.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+}
